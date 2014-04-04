3af3284cfb47163d6e6b2b5ea8d84bb5e3f64743
Sunny Luthra <sunny@mrova.com>
2014-04-04 12:07:48 +0530

    First Push

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+define('APP_ROOT', dirname(__DIR__));
+define('APP_ENV', getenv('APPLICATION_ENV'));
+if(file_exists(APP_ROOT . '/env/'.APP_ENV.'.php')){
+	require APP_ROOT . '/env/'.APP_ENV.'.php';
+}else{
+	require APP_ROOT . '/env/sny-dev.php';
+}
+$table_prefix  = 'predictor_';
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', APP_ROOT . '/public/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'settings.php');
---
+++ public/settings.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+<?php
+// Include ezSQL core
+include_once "../db/ez_sql_core.php";
+// Include ezSQL database specific component
+include_once "../db/ez_sql_mysql.php";
+// Initialise database object and establish a connection
+// at the same time - db_user / db_password / db_name / db_host
+$db = new ezSQL_mysql(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST) or die("Error connecting database.");
